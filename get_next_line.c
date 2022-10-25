e812eca54959867eba5c41b9e2ce2733c6136616
Felipe <andrade.adsf@gmail.com>
2022-10-24 21:39:46 -0300

    little work done

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 11:13:53 by coder             #+#    #+#             */
-/*   Updated: 2022/10/22 12:10:33 by coder            ###   ########.fr       */
+/*   Updated: 2022/10/24 20:23:27 by feandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,39 @@
 char	*get_next_line(int fd)
 {
 	char	*buffer;
+	char	*lastline;
 	static int	i_buffer = 0;
-	char *lastline;
+	int		not_end;
 
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	read(fd, buffer, BUFFER_SIZE);
+	not_end = 0;
+	while (not_end == 0)
+	{
+		read(fd, BUFFER_SIZE, 1);
+	}
+
+
+
+
+// salva o que foi lido pelo buffer em uma variavel e retorna
 	while (buffer[i_buffer] != '\0' || buffer[i_buffer])
 		i_buffer++;
 	lastline = malloc((sizeof(char) * i_buffer) + 1);
 	lastline[i_buffer + 1] = '\0';
 	while (i_buffer >= 0)
-	{
+
 		lastline[i_buffer] = buffer[i_buffer];
 		i_buffer--;
 	}
 	return (lastline);
+}
+
+char	*read_file(int fd)
+{
+	read(fd, buffer, BUFFER_SIZE);
+	
+	while (not_end == 0)
+	{
+		read_file(fd);
+	}
 }
