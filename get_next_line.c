e746fc9a41708a84f9d212725d05d00522864b67
Felipe <andrade.adsf@gmail.com>
2022-11-09 19:02:33 -0300

    while ended and added 0 on buffer end

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 11:13:53 by coder             #+#    #+#             */
-/*   Updated: 2022/11/08 19:53:47 by feandrad         ###   ########.fr       */
+/*   Updated: 2022/11/09 19:01:56 by feandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,25 @@
 char	*get_next_line(int fd)
 {
 	char	*buffer;
-	int		not_end;
-	char	*result;
-	int		line_end
+	int		end;
+	char	*dst;
+	ssize_t	rread;
 
 	if (!buffer)
 		buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	while (not_end == 1)
+	while (end == 0)
 	{
-		read(fd, buffer, BUFFER_SIZE);
-		if (!result)
-			result = malloc((BUFFER_SIZE +1) * sizeof(char));
-		buffercat(result, buffer);
-		line_end = ft_strlen(result)
-		if	(line_end == '\n' || line_end == '\0')
-			not_end == 0;
+		rread = read(fd, buffer, BUFFER_SIZE);
+		buffer[rread] = '\0';
+		if (!dst)
+			dst = malloc((BUFFER_SIZE + 1) * sizeof(char));
+		buffercat(dst, buffer);
+		if (rread <=  0)
+			return (NULL);
+		else
+			end = check_endline(dst);
 	}
-	return (result);
+	return (end);
 }
+
+char	find_line()
