8f550f17f816d4c008926d74e5c4bef136d6e77f
Felipe <andrade.adsf@gmail.com>
2022-11-15 05:35:35 -0300

    working with current functions

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+int	ft_strlen(const char *str, char stopper);
+void	swap_dest(char *dst, int dst_size);
+void	buffercat(char *dst, const char *buffer);
+void	ft_strlcpy(char *dst, const char *src, int size, char stopper);
+int	check_endline(char str);
+char	*get_next_line(int fd);
+char	*show_result(const char *dst);
+
+#endif
