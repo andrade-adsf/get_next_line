6e393ed1edfc1c093723516f0c660003f7a8d05e
Felipe <andrade.adsf@gmail.com>
2022-11-21 19:17:54 -0300

    prot updated

--- get_next_line.h
+++ get_next_line.h
@@ -9,14 +9,15 @@
 # include <fcntl.h>
 # include <stdlib.h>
 
-char	*get_next_line(int fd)
-int	read_file(char **aux_p, char **line_p, int fd)
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
-char	*ft_strdup(const char *s1)
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-size_t	ft_strlen(const char *c)
-char	*ft_strjoin(char const *s1, char const *s2)
-void	*ft_calloc(size_t count, size_t size)
-void	ft_bzero(void *s, size_t n)
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		load_buffer(char *buf, int fd);
+int		read_file(char **aux_p, char **line_p, int fd);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *c);
+void	*ft_calloc(size_t count, size_t size);
+void	ft_bzero(void *s, size_t n);
 
 #endif
